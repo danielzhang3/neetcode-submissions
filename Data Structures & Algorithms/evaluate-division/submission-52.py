9910874881b3f6ee5f48e1fb4af70c715ba9321d
Daniel Zhang <138730381+danielzhang3@users.noreply.github.com>
2026-06-27 10:23:15 -0400

    Add: evaluate-division - submission-52

---
+++ Data Structures & Algorithms/evaluate-division/submission-52.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+
+        for (A, B), k in zip(equations, values): 
+            graph[A].append((B, k))
+            graph[B].append((A, 1.0 / k))
+        
+        def dfs(c, d, visited):
+            if c not in graph or d not in graph: 
+                return -1.0
+
+            if c == d: 
+                return 1.0
+
+            visited.add(c)
+            for nei, weight in graph[c]: 
+                if nei not in visited: 
+                    res = dfs(nei, d, visited)
+                    if res != -1.0: 
+                        return weight * res
+            return -1.0
+
+        res = []
+        for c, d in queries: 
+            res.append(dfs(c, d, set()))
+
+        return res 
+            
+        
