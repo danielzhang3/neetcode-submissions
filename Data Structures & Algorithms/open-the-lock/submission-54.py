656617682b4a8044648ae3b94d677b05035dc23f
Daniel Zhang <138730381+danielzhang3@users.noreply.github.com>
2026-07-05 09:15:42 -0400

    Add: open-the-lock - submission-54

---
+++ Data Structures & Algorithms/open-the-lock/submission-54.py
@@ -0,0 +1,39 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        dead = set()
+        for combo in deadends: 
+            dead.add(combo)
+        
+        if "0000" in dead: 
+            return -1
+        q = deque()
+        q.append("0000")
+        visited = set()
+        visited.add("0000")
+
+        def neighbors(code): 
+            res = []
+            for i in range(4): 
+                digit = int(code[i])
+                for move in (+1, -1): 
+                    next_digit = (digit + move) % 10
+                    nxt = code[:i] + str(next_digit) + code[i + 1:]
+                    res.append(nxt)
+            return res
+        
+        moves = 0
+        
+        while q: 
+            for _ in range(len(q)): 
+                combo = q.popleft()
+                if combo == target: 
+                    return moves
+                for nei in neighbors(combo): 
+                    if nei not in dead and nei not in visited: 
+                        q.append(nei)
+                        visited.add(nei)
+            moves += 1
+        
+        return -1
+
+        
