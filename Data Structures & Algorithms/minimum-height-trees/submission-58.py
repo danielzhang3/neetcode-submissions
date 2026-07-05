86cf7b5483ac5c2a6e25b990a8adbe7d7a13f2af
Daniel Zhang <138730381+danielzhang3@users.noreply.github.com>
2026-07-05 10:01:46 -0400

    Add: minimum-height-trees - submission-58

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-58.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1: 
+            return [0]
+        
+        graph = defaultdict(list)
+        edge_cnt = {}
+        q = deque()
+
+        for u, v in edges: 
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        for src, nei in graph.items(): 
+            edge_cnt[src] = len(nei)
+            if len(nei) == 1: 
+                q.append(src)
+        
+        while q: 
+            if n <= 2: 
+                return list(q)
+            for _ in range(len(q)): 
+                cur = q.popleft()
+                n -= 1
+                for nei in graph[cur]: 
+                    edge_cnt[nei] -= 1
+                    if edge_cnt[nei] == 1: 
+                        q.append(nei)
+        
+        
+        
