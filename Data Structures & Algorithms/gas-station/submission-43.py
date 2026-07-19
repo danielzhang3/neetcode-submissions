03a550c90a2e5790729a34301df18ec0ade6b17a
Daniel Zhang <138730381+danielzhang3@users.noreply.github.com>
2026-07-18 22:26:21 -0400

    Add: gas-station - submission-43

---
+++ Data Structures & Algorithms/gas-station/submission-43.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start, tank, total = 0, 0, 0
+        
+        for i in range(len(gas)): 
+            diff = gas[i] - cost[i]
+            tank += diff
+            total += diff
+
+            if tank < 0: 
+                start = i + 1
+                tank = 0
+        
+        return start if total >= 0 else -1
+        
