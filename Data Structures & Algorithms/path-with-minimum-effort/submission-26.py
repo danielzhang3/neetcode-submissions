1f281e027e92fc9bfd047872aa73368da6f7570e
Daniel Zhang <138730381+danielzhang3@users.noreply.github.com>
2026-07-22 21:10:19 -0400

    Add: path-with-minimum-effort - submission-26

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-26.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        ROWS, COLS = len(heights), len(heights[0])
+        visited = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        heap = []
+        heapq.heappush(heap, (0, 0, 0))
+
+        while heap: 
+            eff, r, c = heapq.heappop(heap)
+            if (r, c) in visited: 
+                continue
+            if (r == ROWS - 1 and c == COLS - 1): 
+                return eff
+            visited.add((r, c))
+            for dr, dc in directions: 
+                row, col = r + dr, c + dc
+                if (row in range(ROWS) and col in range(COLS) and 
+                    (row, col) not in visited): 
+                    newEff = abs(heights[row][col] - heights[r][c])
+                    maxEff = max(newEff, eff)
+                    heapq.heappush(heap, (maxEff, row, col))
+        
